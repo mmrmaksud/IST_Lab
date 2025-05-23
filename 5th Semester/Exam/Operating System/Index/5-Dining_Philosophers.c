fc54d750023ab3c6025a8837577f4c6aac123da2
Maksud <95296319+mmrmaksud@users.noreply.github.com>
2025-05-23 20:07:37 +0600

    Add files via upload

---
+++ 5th Semester/Exam/Operating System/Index/5-Dining_Philosophers.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define N 4 // Number of philosophers
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+#define MAX_EAT_TIMES 3 // Each philosopher eats this many times
+
+int state[N];               // Track philosopher state
+int phil[N] = {0, 1, 2, 3}; // Philosopher identifiers
+
+sem_t mutex; // Semaphore for critical section
+sem_t S[N];  // Semaphore for each philosopher
+
+void test(int philosopher)
+{
+    if (state[philosopher] == HUNGRY &&
+        state[(philosopher + 3) % N] != EATING &&
+        state[(philosopher + 1) % N] != EATING)
+    {
+
+        state[philosopher] = EATING;
+        printf("Philosopher %d takes forks %d and %d and starts EATING\n",
+               philosopher, (philosopher + 3) % N, philosopher);
+        sleep(2);
+        printf("Philosopher %d finishes eating\n", philosopher);
+        sem_post(&S[philosopher]); // Allow philosopher to eat
+    }
+}
+
+void take_fork(int philosopher)
+{
+    sem_wait(&mutex);
+    state[philosopher] = HUNGRY;
+    printf("Philosopher %d is HUNGRY\n", philosopher);
+    test(philosopher);
+    sem_post(&mutex);
+    sem_wait(&S[philosopher]);
+    sleep(1);
+}
+
+void put_fork(int philosopher)
+{
+    sem_wait(&mutex);
+    state[philosopher] = THINKING;
+    printf("Philosopher %d puts forks %d and %d down and starts THINKING\n",
+           philosopher, (philosopher + 3) % N, philosopher);
+    test((philosopher + 3) % N);
+    test((philosopher + 1) % N);
+    sem_post(&mutex);
+}
+
+void *philosopher(void *num)
+{
+    int *i = num;
+    for (int eat_count = 0; eat_count < MAX_EAT_TIMES; eat_count++)
+    {
+        printf("Philosopher %d is THINKING\n", *i);
+        sleep(1);
+        take_fork(*i);
+        sleep(1);
+        put_fork(*i);
+    }
+    printf("Philosopher %d has finished eating and is leaving.\n", *i);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t thread_id[N];
+
+    sem_init(&mutex, 0, 1);
+    for (int i = 0; i < N; i++)
+        sem_init(&S[i], 0, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        pthread_create(&thread_id[i], NULL, philosopher, &phil[i]);
+        printf("Philosopher %d is created\n", i);
+    }
+
+    for (int i = 0; i < N; i++)
+        pthread_join(thread_id[i], NULL);
+
+    printf("All philosophers have finished eating. Simulation ends.\n");
+    return 0;
+}
---
+++ 5th Semester/Exam/Operating System/Question/9-FCFS.c
@@ -0,0 +1,104 @@
+// Write a C program to simulate the CPU scheduling algorithm - First Come First Serve.
+
+#include <stdio.h>
+void fwt(int process[], int n, int bt[], int wt[])
+{
+    wt[0] = 0;
+    int i;
+    for (i = 1; i < n; i++)
+        wt[i] = bt[i - 1] + wt[i - 1];
+}
+void ftat(int process[], int n, int bt[], int wt[], int tat[])
+{
+    int i;
+    for (i = 0; i < n; i++)
+        tat[i] = bt[i] + wt[i];
+}
+void fat(int processes[], int n, int bt[])
+{
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+    fwt(processes, n, bt, wt);
+    ftat(processes, n, bt, wt, tat);
+    printf("Process\t Burst Time\tWaiting Time\tTurnaround Time\n");
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf(" P%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+    }
+    printf("\nAverage Waiting Time (W.T): %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time (T.A.T): %.2f\n", (float)total_tat / n);
+}
+int main()
+{
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    int bt[n];
+    printf("Enter burst time for each process:\n");
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
+    fat(NULL, n, bt);
+    return 0;
+}
+
+/*Correction: 1-FCFS.c
+
+#include <stdio.h>
+
+void fwt(int process[], int n, int bt[], int wt[]) {
+    wt[0] = 0;
+    for (int i = 1; i < n; i++)
+        wt[i] = bt[i - 1] + wt[i - 1];
+}
+
+void ftat(int process[], int n, int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++)
+        tat[i] = bt[i] + wt[i];
+}
+
+void fat(int processes[], int n, int bt[]) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    fwt(processes, n, bt, wt);
+    ftat(processes, n, bt, wt, tat);
+
+    printf("Process\t Burst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf(" P%d\t\t%d\t\t%d\t\t%d\n", processes[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time (W.T): %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time (T.A.T): %.2f\n", (float)total_tat / n);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int bt[n], processes[n];
+
+    printf("Enter burst time for each process:\n");
+    for (int i = 0; i < n; i++) {
+        processes[i] = i + 1;  // Assign process ID
+        printf("P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
+
+    fat(processes, n, bt);
+    return 0;
+}
+
+Fixes and Improvements
+✅ Added a processes[] array to store process IDs instead of passing NULL.
+✅ Fixed fat(NULL, n, bt); to fat(processes, n, bt);
+✅ Used int instead of declaring i separately to improve readability.
+*/
