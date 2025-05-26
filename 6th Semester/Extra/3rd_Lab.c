417a3f695c99300e8892d683e0080a2ed17e4e1c
Maksud <95296319+mmrmaksud@users.noreply.github.com>
2025-05-26 11:35:43 +0600

    Add files via upload

---
+++ 6th Semester/Extra/3rd_Lab.c
@@ -0,0 +1,22 @@
+#include<reg51.h>
+void Delay();
+void main()
+{
+	while(1)
+	{
+		P1=0xAA;
+		Delay();
+		P1=0x55;
+		Delay();
+	}
+}
+void Delay()
+{
+	int i,j;
+	for(i=0;i<10;i++)
+	{
+		for(j=0;j<10000;j++)
+		{
+		}
+	}
+}
